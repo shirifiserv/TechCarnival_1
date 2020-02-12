0933bd4acc46d1f695b2cb6394c283a508b6886f
Vasanti Kumbhar <Vasanti.Kumbhar@fiserv.com>
2020-02-12 03:17:44 -0500

    added unit test cases.

---
+++ TechnologyCarnival/MVCImageSliderFromFolder/TechCarnivalUnitTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TechCarnivalUnitTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TechCarnivalUnitTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7d4dccba-ea58-4304-bcf0-7c0b223e6366")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TechnologyCarnival/MVCImageSliderFromFolder/TechCarnivalUnitTesting/UnitTest1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TechCarnivalUnitTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        readonly string PathToFile = @"MVCImageSliderFromFolder";
+        readonly string FileType = "config";
+        readonly string CurrentTestDirectory = "MVCImageSliderFromFolder";
+        [TestMethod]
+        public void TestConfigFiles()
+        {
+            var configFiles = new List<string>();
+            var errors = new List<string>();
+            var currentPath = AppDomain.CurrentDomain.BaseDirectory;
+            var configPath = currentPath.Substring(0, currentPath.LastIndexOf(CurrentTestDirectory));
+            var finalPath = Path.Combine(configPath, PathToFile);
+            var files = Directory.GetFiles(finalPath);
+
+            foreach (var file in files.Where(f => f.Contains(FileType)))
+            {
+                configFiles.Add(file);
+            }
+
+            foreach (var file in configFiles)
+            {
+                try
+                {
+                    var doc = XDocument.Load(file);
+                }
+                catch (Exception e)
+                {
+                    errors.Add($"Error loading {file}: {e.Message}");
+                }
+
+            }
+
+            if (errors.Any())
+            {
+                string errorMessage = string.Empty;
+                foreach (var error in errors)
+                {
+                    errorMessage += error;
+                }
+                Assert.Fail($"Check your config file: {errorMessage}");
+            }
+        }
+    }
+}
